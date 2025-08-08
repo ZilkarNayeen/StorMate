36b4c2f26381a810b98a74d366e0e33a2bd0cae0
Hossain Md Nayeen Zilkar <zilkarnayeen12345@gmai.com>
2025-08-08 18:06:19 +0600

    added new file

--- frontend/src/utils/Root.jsx
+++ frontend/src/utils/Root.jsx
@@ -1,28 +1,34 @@
-// src/utils/Root.jsx
 
-import { useEffect } from "react"; // Changed 'use' to 'useEffect'
+import { useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router"; // Corrected import from "react-router" to "react-router-dom"
+import { useNavigate } from "react-router";
 
 const Root = () => {
-    const { user } = useAuth();
-    const navigate = useNavigate();
+  const { user, logout } = useAuth(); 
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (user) {
-            if (user.role === 'admin') {
-                navigate('/admin-dashboard');
-            } else if (user.role === 'customer') {
-                navigate('/customer-dashboard');
-            } else {
-                navigate('/login');
-            }
-        } else {
-            navigate('/login');
-        }
-    }, [user, navigate]);
+  useEffect(() => {
+    const token = localStorage.getItem("pos-token");
 
-    return (null); // This will render a blank page while the redirect happens
+    
+    if (!user || !token) {
+      logout(); 
+      navigate("/login");
+      return;
+    }
+
+    // Redirect based on role
+    if (user.role === "admin") {
+      navigate("/admin-dashboard");
+    } else if (user.role === "customer") {
+      navigate("/customer-dashboard");
+    } else {
+      logout();
+      navigate("/login");
+    }
+  }, [user, navigate, logout]);
+
+  return null;
 };
 
 export default Root;
